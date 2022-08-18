3ab9acd29768a035fa4c79aa8333dc083ce2110e
tmsree <87114837+tmsree@users.noreply.github.com>
2022-08-19 07:45:25 +0800

    yes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,11 @@
 // database module
 var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'cdev',
+    database: 'cilla'
+});
 var config = {
     host: 'localhost',
     user: 'root',
@@ -9,6 +15,7 @@ var config = {
 
 // init database
 var pool = mysql.createPool(config);
+
 
 //Fetch data
 function RunQuery(sql, callback) {
@@ -31,6 +38,11 @@ function ShowErrors(err) {
     throw err;
 }
 
+connection.connect(err => {  // test out connetion and console.log error if there is one
+    if (err) throw err;
+    console.log('Connected To AWS DB');
+}); 
+module.exports = connection;
 module.exports = {
     RunQuery: RunQuery
 };
