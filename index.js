37cd03c1d072d4a7f328171196c931fd4edf6fbf
tmsree <87114837+tmsree@users.noreply.github.com>
2022-08-19 06:55:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,26 +17,7 @@ const port= 3000;
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    debug('Listening on ' + bind);
+var server = http.createServer(app)
 }
 
 
